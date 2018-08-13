46a36db7f12f195c67ee87ab7728dde7b8aa29e7
unknown <gerardohernandezmoreno180@gmail.com>
2018-08-13 18:19:47 -0500

    Se genera el repositorio de Banners para vincular el modelo con la tabla y poder realizar los query mediante JPA

--- src/main/java/applinet/eglobal/app/repository/BannersRepository.java
+++ src/main/java/applinet/eglobal/app/repository/BannersRepository.java
@@ -1,11 +1,13 @@
 package applinet.eglobal.app.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import applinet.eglobal.app.model.Banner;
 
 @Repository
-public interface BannersRepository extends JpaRepository<Banner, Integer>  {
-
+public interface BannersRepository extends JpaRepository<Banner, Integer> {
+	List<Banner> findByEstatus(String estatus);
 }
